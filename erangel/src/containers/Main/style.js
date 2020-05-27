8393157843b21e40c0eb83e097bedb0eee58d877
YeYerin <parkyerin30@gmail.com>
2020-05-28 01:10:22 +0900

    Update MainPage

---
+++ erangel/src/containers/Main/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    position: absolute;
+    top: 13.13rem;
+    right: 30.63rem;
+    font-size: 1.563rem;
+    font-weight: 800;
+    font-family: NanumSquare;
+    line-height: 1.6;
+    letter-spacing: 7.5px;
+    text-align: right;
+    color: rgba(255, 255, 255, 0.8);
+`
